fd6db2d5ca60ca785489be31f8551e9c3dc283d5
Parashuram <parashurampawar56@gmail.com>
2026-03-10 14:19:54 +0530

    add program: [parashuram] program to upload json file to s3 bucket

---
+++ scripts/upload_data.py
@@ -0,0 +1,43 @@
+# Import statements
+import os
+import logging
+from botocore.exceptions import *
+from scripts.csv_to_json import csv_to_json_func
+from config.s3_config import get_s3_client, BUCKET_NAME
+
+logging.basicConfig(level=logging.INFO)
+
+def upload_file_to_s3(local_file = 'data/student_performance.csv', bucket_name = BUCKET_NAME):
+    s3 = get_s3_client()
+    json_file = csv_to_json_func(local_file)
+    if not os.path.exists(json_file):
+        logging.error("File doesn't exist. please check the path")
+        return
+    try:
+        s3.upload_file(
+            json_file,
+            bucket_name,
+            'student_performance.json'
+        )
+        logging.info(f"Upload successful: s3://{bucket_name}/student_performance.json")
+    except NoCredentialsError:
+        logging.error("AWS credentials not available.")
+    except Exception as e:
+        logging.error(f"Upload failed: {e}")
+
+def list_s3_files(bucket_name = BUCKET_NAME):
+    s3 = get_s3_client()
+    try:
+        response = s3.list_objects_v2(Bucket = bucket_name)
+        if 'Contents' in response:
+            logging.info(f"Files in bucket '{bucket_name}':")
+            for obj in response['Contents']:
+                logging.info(f" - {obj['Key']}")
+        else:
+            logging.info(f"No files found in bucket '{bucket_name}'.")
+    except Exception as e:
+        logging.error(f"Failed to list files: {e}")
+
+if __name__ == "__main__":
+    upload_file_to_s3()
+    list_s3_files()
