ba60463c73d18f7ed67032ad4d16224b52db3fc1
Parashuram <parashurampawar56@gmail.com>
2026-03-10 12:18:59 +0530

    add program: [parashuram] added s3 configuration program

---
+++ config/s3_config.py
@@ -0,0 +1,21 @@
+import os
+import boto3
+from dotenv import load_dotenv
+
+# -----------------------------------------------------------------------
+# Load environment variables
+# -----------------------------------------------------------------------
+load_dotenv()
+
+# -----------------------------------------------------------------------
+# AWS configuration
+# -----------------------------------------------------------------------
+AWS_REGION = os.getenv("AWS_REGION")
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+
+# -----------------------------------------------------------------------
+# client initialization functions
+# -----------------------------------------------------------------------
+def get_s3_client():
+    session = boto3.Session(region_name=AWS_REGION)
+    return session.client("s3")
