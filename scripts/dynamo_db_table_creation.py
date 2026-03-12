cb1cf6baa023ce48a9fc571e792ff02620648b4f
Parashuram <parashurampawar56@gmail.com>
2026-03-12 15:59:53 +0530

    modify program: [parashuram] updated program to create dynamodb table

--- scripts/dynamo_db_table_creation.py
+++ scripts/dynamo_db_table_creation.py
@@ -1,10 +1,13 @@
 import logging
 from config.s3_config import dynamodb_client
 
+# Initialize logging
 logging.basicConfig(level=logging.INFO)
 
+# Initialize dynamodb client
 dynamodb = dynamodb_client()
 
+# Table creation logic
 logging.info("Table creation started...")
 try:
     table = dynamodb.create_table(
@@ -21,9 +24,8 @@ try:
         BillingMode="PAY_PER_REQUEST"
     )
     logging.info("Table created successfully...")
-
 except dynamodb.exceptions.ResourceInUseException:
     logging.info("Table already exists")
-
+    
 dynamodb.get_waiter('table_exists').wait(TableName='student_performance')
 logging.info("Table is now active")
