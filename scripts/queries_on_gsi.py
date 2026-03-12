b5168e4a833fb90d7eb9af72516624ab64f7dc00
Parashuram <parashurampawar56@gmail.com>
2026-03-12 16:08:37 +0530

    modify program: [parashuram] updated program added docstrings

--- scripts/queries_on_gsi.py
+++ scripts/queries_on_gsi.py
@@ -7,6 +7,10 @@ dynamodb = dynamodb_resource()
 table = dynamodb.Table("student_performance")
 
 def query_top_students_by_gradea():
+    '''
+    Retrieve the top 10 students with grade 'A' using the
+    'grade_total_score_index', ordered by highest total score.
+    '''
     logging.info("Querying top students with grade A...")
     result = table.query(
         IndexName="grade_total_score_index",
@@ -21,6 +25,10 @@ def query_top_students_by_gradea():
     logging.info("Finished querying top students with grade A.")
 
 def query_students_with_highest_scores():
+    '''
+    Retrieve students with the highest total score among
+    those who have grade 'A'.
+    '''
     logging.info("Querying students with highest scores...")
     result = table.query(
         IndexName="grade_total_score_index",
@@ -37,17 +45,18 @@ def query_students_with_highest_scores():
     logging.info("Students with highest scores:")
     for item in result2["Items"]:
         print(f"Student ID: {item['student_id']}, Grade: {item['grade']}, Total Score: {item['total_score']}")
-    # LastEvaluatedKey = result.get("LastEvaluatedKey")
-    # while LastEvaluatedKey:
-    #     result = table.query(
-    #         IndexName="grade_total_score_index",
-    #         KeyConditionExpression = Key("grade").eq("A") & Key("total_score").eq(highest_score),
-    #         ProjectionExpression="student_id, grade, total_score",
-    #         ExclusiveStartKey=LastEvaluatedKey
-    #     )
-    #     for item in result["Items"]:
-    #         print(f"Student ID: {item['student_id']}, Grade: {item['grade']}, Total Score: {item['total_score']}")
-    #     LastEvaluatedKey = result.get("LastEvaluatedKey")
+        
+    LastEvaluatedKey = result.get("LastEvaluatedKey")
+    while LastEvaluatedKey:
+        result = table.query(
+            IndexName="grade_total_score_index",
+            KeyConditionExpression = Key("grade").eq("A") & Key("total_score").eq(highest_score),
+            ProjectionExpression="student_id, grade, total_score",
+            ExclusiveStartKey=LastEvaluatedKey
+        )
+        for item in result["Items"]:
+            print(f"Student ID: {item['student_id']}, Grade: {item['grade']}, Total Score: {item['total_score']}")
+        LastEvaluatedKey = result.get("LastEvaluatedKey")
     logging.info("Finished querying students with highest scores.")
 
 if __name__ == "__main__":
