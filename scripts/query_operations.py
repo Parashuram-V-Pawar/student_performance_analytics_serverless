b58cd66e2f1788a494c84ac4d8f83f8ad92dec4f
Parashuram <parashurampawar56@gmail.com>
2026-03-12 16:06:43 +0530

    modify program: [parashuram] updated program added docstrings

--- scripts/query_operations.py
+++ scripts/query_operations.py
@@ -3,13 +3,17 @@ from decimal import Decimal
 from boto3.dynamodb.conditions import *
 from config.s3_config import dynamodb_resource
 
-
+# Initialize logging
 logging.basicConfig(level=logging.INFO)
 
+# Initialize DynamoDB resource and table
 dynamodb = dynamodb_resource()
 table = dynamodb.Table("student_performance")
 
 def query_attendance_morethan_90():
+    '''
+    Retrieve and display students whose attendance percentage is greater than 90%.
+    '''
     logging.info("Querying students with attendance more than 90%...")
     result = table.scan(
         FilterExpression=Attr("attendance_percentage").gt(Decimal("90.0"))
@@ -21,6 +25,9 @@ def query_attendance_morethan_90():
 
 
 def query_weekly_study_hours_morethan_10():
+    '''
+    Retrieve and display students whose weekly self-study hours exceed 10.
+    '''
     logging.info("Querying students with weekly study hours more than 10...")
     result = table.scan(
         FilterExpression=Attr("weekly_self_study_hours").gt(Decimal("10.0"))
@@ -32,6 +39,9 @@ def query_weekly_study_hours_morethan_10():
 
 
 def query_students_in_excellent_performance():
+    '''
+    Retrieve and display students categorized under 'Excellent' performance.
+    '''
     logging.info("Querying students with excellent performance...")
     result = table.scan(
         FilterExpression=Attr("performance_category").eq("Excellent")
