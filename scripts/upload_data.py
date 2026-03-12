60584bc794b85bdda2f6f30049be9490d7c5e941
Parashuram <parashurampawar56@gmail.com>
2026-03-12 16:04:26 +0530

    modify program: [parashuram] updated program added docstrings

--- scripts/upload_data.py
+++ scripts/upload_data.py
@@ -5,16 +5,24 @@ from botocore.exceptions import *
 from scripts.csv_to_json import csv_to_json_func
 from config.s3_config import get_s3_client, BUCKET_NAME
 
+# Initialize logging
 logging.basicConfig(level=logging.INFO)
 
 def upload_file_to_s3(local_file = 'data/students_records.json', s3_key = "raw", bucket_name = BUCKET_NAME):
+    '''
+    Upload a CSV or JSON file to an S3 bucket.
+    If the input file is CSV, it is converted to JSON before uploading.
+
+    Args:
+        local_file (str): Path to the local file.
+        s3_key (str): S3 prefix (folder) where the file will be stored.
+        bucket_name (str): Target S3 bucket name.
+    '''
     s3 = get_s3_client()
     file_ext = os.path.splitext(local_file)[1].lower()
     try:
-        # If CSV → convert to JSON
         if file_ext == ".csv":
             json_file = csv_to_json_func(local_file)
-        # If JSON → use file directly
         elif file_ext == ".json":
             json_file = local_file
         else:
@@ -37,6 +45,12 @@ def upload_file_to_s3(local_file = 'data/students_records.json', s3_key = "raw",
         logging.error(ve)
 
 def list_s3_files(bucket_name = BUCKET_NAME):
+    '''
+    List all files available in the specified S3 bucket.
+
+    Args:
+        bucket_name (str): Name of the S3 bucket.
+    '''
     s3 = get_s3_client()
     try:
         response = s3.list_objects_v2(Bucket = bucket_name)
